b210682a18e2cf7b73bfaf8ebc10a7f2171a8900
eddiemuhoro <eddiemuhoro@gmail.com>
2022-12-11 13:15:52 +0300

    added deadline component

--- src/components/Mui.js
+++ src/components/Mui.js
@@ -11,7 +11,7 @@ import HomeNavBar from './homePage/HomeNavBar';
 import Popup from 'reactjs-popup';
 import { Email, LocationOn, Phone, WhatsApp } from '@mui/icons-material';
 import ReactWhatsapp from 'react-whatsapp';
-import Counter from '../counter/Counter';
+import Timer from './counter/Counter';
 
 
 
@@ -29,6 +29,7 @@ export default function Mui() {
             })
             
     })
+    const deadline = "12/ 31/ 2022";
     
     return (
         <>
@@ -71,7 +72,7 @@ export default function Mui() {
                         <p><span>Est. Budget</span>Ksh 450</p>
                     </bottom>
                     <div className='job-time'>
-                   <Counter />
+                   <Timer deadline={deadline} />
                     <Popup trigger={<Button variant='contained'>Bid</Button> } modal>
                              <h2>{job.title}</h2>
                             <h3>Connect with {job.employer}</h3>
