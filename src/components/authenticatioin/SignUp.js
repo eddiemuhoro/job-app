957e9336c8bfba29bb71dc2effce945a870517b6
eddiemuhoro <eddiemuhoro@gmail.com>
2022-12-11 23:55:33 +0300

    added password icon

--- src/components/authenticatioin/SignUp.js
+++ src/components/authenticatioin/SignUp.js
@@ -1,23 +1,83 @@
-import { Button, FormControl, TextField } from '@mui/material'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import { Button, FilledInput, FormControl, IconButton, InputAdornment, TextField } from '@mui/material'
 import React from 'react'
 import { Link, Router } from 'react-router-dom'
+import SideBar from './SideBar'
 
 const SignUp = () => {
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+   cpassword:'',
+    showPassword: false,
+  });
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className='signup-form'>    
+    <div className='signup-form'>   
+    <SideBar />
         <FormControl 
          sx={{
             border:'1px solid grey',
             borderRadius:'5px',
             padding:'20px',
-            '& .MuiTextField-root': { m: 1, width: '40ch',  },
+            width:'50%',
+            '& .MuiTextField-root .MuiFilledInput-root': { m: 1, width: 'auto',  },
           }}>
             <h1>Sign Up</h1>
             <TextField type='text' variant='filled' label='Your name'/>
             <TextField type='email' variant='filled' label='Your email'/>
-            <TextField type='password' variant='filled' label='Your password'/>
-            <TextField type='password' variant='filled' label='Confirm password'/>
-            <Link to='/home'><Button fullWidth variant='contained'  >SIGN UP</Button></Link>
+            <FilledInput  variant='filled' label='Your password' placeholder='Your password'
+            id="filled-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility/>}
+                </IconButton>
+              </InputAdornment>
+            }/>
+          
+            <FilledInput
+            variant='filled' placeholder='Confirm password'
+            id="filled-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.cpassword}
+            onChange={handleChange('cpassword')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle cpassword visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility/>}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+            <Link style={{margin:'5px'}} to='/home'><Button fullWidth variant='contained'  >SIGN UP</Button></Link>
             <p>Already have an account? <Link style={{color:'black'}} to='/login'>Login</Link></p>
         </FormControl>  
     </div>
--- src/components/authenticatioin/login.js
+++ src/components/authenticatioin/login.js
@@ -1,23 +1,65 @@
 import React from 'react'
-import { Button, FormControl, TextField } from '@mui/material'
+import { Button, FilledInput, FormControl, IconButton, InputAdornment, TextField } from '@mui/material'
 import { Link } from 'react-router-dom'
+import SideBar from './SideBar'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
 
 
 const Login = () => {
+  const [values, setValues] = React.useState({
+    password: '',
+   cpassword:'',
+    showPassword: false,
+  });
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className='signup-form'>
+       <div className='login-sidebar'>
+            <img src='https://t4.ftcdn.net/jpg/02/60/53/37/360_F_260533737_N1QkCY09mwIy7A0Yph79lkqCl0iB2mvF.jpg' alt='login'/>
+      </div> 
         <FormControl 
       
       sx={{
          border:'1px solid grey',
          borderRadius:'5px',
-         padding:'20px',
-         '& .MuiTextField-root': { m: 1, width: '40ch',  },
+          padding:'20px',
+          width:'50%',
+         '& .MuiTextField-root .MuiFilledInput-root': { m: 1, width: '100%',  },
        }}>
          <h1>Log In</h1>
          <TextField type='email' variant='filled' label='Your email'/>
-         <TextField type='password' variant='filled' label='Your password'/>
-         <Link style={{width:'100%', margin:"auto"}} to='/home'><Button fullWidth variant='contained'>LOG IN</Button></Link>
+         <FilledInput  variant='filled' label='Your password' placeholder='Your password'
+            id="filled-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility/>}
+                </IconButton>
+              </InputAdornment>
+            }/>
+         <Link  style={{width:'100%', margin:'5px'}} to='/home'><Button fullWidth variant='contained'>LOG IN</Button></Link>
          <p>Don't have an account? <Link style={{color:'black'}} to='/'>Sign Up</Link></p>
      </FormControl>
     </div>
