58049ff49f0e548d140fc37cd0bfd17cba0bdc07
eddiemuhoro <eddiemuhoro@gmail.com>
2022-12-21 03:07:41 +0300

    live api-link

--- src/react-redux/features/auth/authService.js
+++ src/react-redux/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = ' http://localhost:5000/';
+const API_URL = ' https://expressjs-production-e1ab.up.railway.app/';
 
 const register = async(userData)=>{
     const response = await axios.post(API_URL + 'register', userData)
