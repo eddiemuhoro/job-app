388eb42d1d7023e4628345782818c18a5b53e213
eddiemuhoro <eddiemuhoro@gmail.com>
2023-01-11 09:39:41 +0300

    added profile

--- src/components/about/Team.js
+++ src/components/about/Team.js
@@ -32,7 +32,7 @@ const Team = () => {
             name:'Carol Muthoni',
             desc: 'Student in Dekut pursuing BSC computer science. She is interested in machine learning using python',
             position:'Secretary',
-            image:'https://lh3.googleusercontent.com/_AnNcc366eInYm95PKWW94lCuCqvXrX23vNmzTHYMumXeIePiK4WWebRwMZ8UZiazldQCtNzD562ufVkclpuHDB0zvAfzIQoJeTTYZTSInJqKeApdxKWSCoL1tcxjERRHlb0KwG9FPRb0yMqYqOyz7rX_VUEL9o500USrGZMb4Nt1kRJUGbqW3LDF6qRbC07wwuRP9Ypu-6veGaDpwquP1bUH8U9_pXOzDRGAluqatBc938Vk3wvb1iE9rqZI8MmyE_ZZyKyOdrZVcC3qld5S5Eb-ieozL_KgcepAVJei95JCjA6v1j_e292Mq2RRW4WLqPyNk0-ijTBRuuQ6Ikdq29zL1DrqmrYmXUD45_qYu1D9QV4i9j0WtVvQSms6yZqHm84Ybh5yREHj6CeQcyO9kBOp5rNtV0J1HoHNMvAYyo0XlgxLObkb1XZP83B2IOwv08H8eZHG_dE4pKc3CaJe6QRl2_7tFoDcHK48ZrWFxK55wP42PUU66iuINTfacBC8oRQ_pGGQckV17kwVJ6bUjYgdgfFdQyW4Gu76CidbM2pyGL-MfPyICxTkz4QU3nGdvNqsv06PmWzmQXzaG1MUjUcDW9Xk8-dFlVwMPVH1z7-fp5lJ0Ovj2DA-rtFI0CostOzXvwbo5vukUzMkjFfqCn_j58K1OtjaKuk0kDRiLemQiBbH37StE9kb-Kq5fQ5FBHuXWU9S8J1BXPa6x0UQnz93ADW4OcPND2dok9pNkGEjOu2M0lbsOnGmLWgb7cqSgCRgVlUPKQC_KqVewFDfcPXEcxfvqpQK44q_2PhQ2gKk2SZJs0pqhdkaASu0zylbOzW-GGMxH_vSaAjJIrlv4z1wcU11UCIsgCtejA0cY9UtwmpnKi3QetydHKogVoB3sLC1Fsn_fGYNULoFxW_wRaAyYtv3d2kTDS51j0-1nk_O53cYOH1F1kKDYvX5SyAmug60oomCZCT3ngYoqIf=w779-h766-no?authuser=0'
+            image:'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png'
         },
         {
             id:5,
