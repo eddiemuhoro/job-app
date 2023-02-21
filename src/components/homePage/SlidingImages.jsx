879550501895a37413ff5c448ce70454b552dba2
eddiemuhoro <eddiemuhoro@gmail.com>
2023-02-21 22:46:53 +0300

    added new images

--- src/components/homePage/SlidingImages.jsx
+++ src/components/homePage/SlidingImages.jsx
@@ -4,8 +4,8 @@ const SlidingImages = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [images, setImages] = useState([
     'http://localhost:3000/static/media/image.9779b4d9935fa4e195b1.png',
-    'https://super-monitoring.com/blog/wp-content/uploads/2019/07/freelancers.jpg',
-    'https://idreamcareer.com/wp-content/uploads/2020/09/freelance-jobs-for-students.webp'
+    'https://sophicalcontent.com/wp-content/uploads/2021/07/firmbee-com-eMemmpUojlw-unsplash.jpg',
+    'https://images.unsplash.com/photo-1604933762023-7213af7ff7a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGZyZWVsYW5jZXxlbnwwfHwwfHw%3D&w=1000&q=80'
   ]);
 
   useEffect(() => {
