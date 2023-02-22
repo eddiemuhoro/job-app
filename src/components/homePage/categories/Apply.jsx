fdb4e958334f4fda20988428395af3f1d691975a
eddiemuhoro <eddiemuhoro@gmail.com>
2023-02-22 19:00:21 +0300

    new component to apply job

---
+++ src/components/homePage/categories/Apply.jsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { BsFlagFill } from 'react-icons/bs'
+import { AiOutlineHeart } from 'react-icons/ai'
+import HomeNavBar from '../HomeNavBar'
+import Footer from '../../Footer/Jfooter'
+const Apply = () => {
+    const [job, setJob] = useState([])
+    const [loading, setLoading] = useState(false)
+    const { id } = useParams()
+    useEffect(() => {
+        setLoading(true)
+        axios.get(`http://localhost:8000/job/${id}`)
+            .then((response) => {
+                setJob(response.data);
+            }).catch((error) => {
+                console.log(error);
+            }).finally(() => {
+                setLoading(false)
+            }
+            )
+    }, [])
+    return (
+        <>
+            <HomeNavBar />
+
+            <div className='application-container'>
+                <h2>Project Details</h2>
+                <div className='application-wrapper'>
+                    <section className='app-left'>
+                        <div className='single-item'>
+                            <div className='category'>
+                                <h2>{job.title}</h2>
+
+                                <p>{job.description}</p>
+                                <p>Time posted</p>
+                                <p>Location</p>
+                            </div>
+                            <div className='description'>
+                                <p>ONLY APPLY IF YOU CAN TURN HTML TO SHOPIFY/FUNNELISH/PAGEFLY/WORDPRESS WITHIN 1-2HOURS
+
+                                    Hi check my html files in the job turn to live website within 1-2 hours
+
+                                    I want a man from india only.
+                                </p>
+                            </div>
+                            <div className='price'>
+                                <p><strong>Ksh 500</strong></p>
+                                <p>Fixed-price</p>
+                            </div>
+                            <div className='expertise'>
+                                <h4 style={{ margin: "0 0 10px 0" }}>Skills and Expertise</h4>
+                                <div className='card-skills'>
+                                    <p>HTML</p>
+                                    <p>CSS</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </section>
+                    <section className='app-right'>
+
+                        <div className='about-client'>
+                            <h4>About Client</h4>
+                            <p>Payment method not verified</p>
+
+                            <p style={{ textDecoration: 'underline', cursor: "pointer" }}>4 jobs posted</p>
+                            <br></br>
+                            <p>Member since 2021</p>
+                        </div>
+                    </section>
+                </div>
+                <div className='similar-jobs'>
+                    <h4>How long will this project take</h4>
+                    {/* SELECTOR */}
+                    <select name="cars" id="cars">
+                        <option value="more than 6 months">More than 6 months</option>
+                        <option value="3 to 6 months">3 to 6 months</option>
+                        <option value="Less than 3 months">Less than 3 months</option>
+                    </select>
+                </div>
+
+                <div className='similar-jobs'>
+                    <h4>Cover Letter</h4>
+                    <textarea name="cover-letter" value='' id="cover-letter" cols="30" rows="10"></textarea>
+                </div>
+
+                <button>Submit proposal </button>
+
+            </div>
+            <Footer />
+        </>
+
+    )
+}
+
+export default Apply
