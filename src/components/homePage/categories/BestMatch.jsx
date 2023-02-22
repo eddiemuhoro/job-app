dd1453f2c0aab064b02b5d5f373f488f8fc60cfd
eddiemuhoro <eddiemuhoro@gmail.com>
2023-02-22 19:00:45 +0300

    help button hover modifications

--- src/components/homePage/categories/BestMatch.jsx
+++ src/components/homePage/categories/BestMatch.jsx
@@ -1,10 +1,22 @@
-import {  ThumbDown, ThumbUp } from '@mui/icons-material'
+import { ThumbDown, ThumbUp } from '@mui/icons-material'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import {GoVerified} from 'react-icons/go'
+import { GoVerified } from 'react-icons/go'
 import { Link } from 'react-router-dom'
+import {BiHelpCircle} from 'react-icons/bi'
 const BestMatch = () => {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleButtonHover = (index) => {
+    setActiveButton(index);
+  };
+
+  const handleButtonLeave = () => {
+    setActiveButton(null);
+  };
+
   //solutiom
+
   const [jobs, setJobs] = useState([])
   const [loading, setLoading] = useState(false)
   useEffect(() => {
@@ -26,41 +38,55 @@ const BestMatch = () => {
   }, [])
   const scrollToTop = () => {
     window.scrollTo(0, 0)
-} 
+  }
   return (
     <>
-      {loading ? <h1>Loading...</h1> : jobs.map((job) => (
+      {loading ? <h1>Loading...</h1> : jobs.map((job, index) => (
         <Link onClick={scrollToTop} to={`/job/${job.id}`} key={job.id}>
-        <div className='job-card'>
-          <div className='card-title'>
-            <h3>{job.title}</h3>
-            <div style={{ display: 'flex' }}>
-              <ThumbUp style={{ color: '#375d06', border: '1px solid gray', padding: '5px', borderRadius: '50%' }} />
-              <ThumbDown style={{ color: '#375d06', border: '1px solid gray', padding: '5px', borderRadius: '50%', marginLeft: "10px" }} />
-            </div>
-          </div>
-          <div className='card-content'>
-            <div className='card-content-left'>
-              <p>We are seeking a talented React Developer with extensive experience in Typescript and Tailwind CSS to join our development team. The successful candidate will work on an ongoing project to develop new features and maintain existing codebases.</p>
-            </div>
-          </div>
-          
-         
-              <div className='card-skills'>
-                {
-                  job.skills.map((skill) => (
-                    <p key={skill}>{skill}</p>
-                  ))
-                }
+          <div className='job-card'>
+            <div className='card-title'>
+              <h3>{job.title}</h3>
+              <div style={{ display: 'flex' }}>
+                <ThumbUp style={{ color: '#375d06', border: '1px solid gray', padding: '5px', borderRadius: '50%' }} />
+                <ThumbDown style={{ color: '#375d06', border: '1px solid gray', padding: '5px', borderRadius: '50%', marginLeft: "10px" }} />
               </div>
+            </div>
+            <div className='card-content'>
+              <div className='card-content-left'>
+                <p>We are seeking a talented React Developer with extensive experience in Typescript and Tailwind CSS to join our development team. The successful candidate will work on an ongoing project to develop new features and maintain existing codebases.</p>
+              </div>
+            </div>
 
-              <div className='payment-verification'>
-                <div className='payment'>
-                  <GoVerified style={{marginRight:"5px"}} />
-                  <p> Payment Verified</p>
+
+            <div className='card-skills'>
+              {
+                job.skills.map((skill) => (
+                  <p key={skill}>{skill}</p>
+                ))
+              }
+            </div>
+
+            <div className='payment-verification'>
+              <div className='payment'>
+                <GoVerified style={{ marginRight: "5px" }} />
+                <p> Payment Verified</p>
+                <div className="button-container">
+                    <p
+                      onMouseEnter={() => handleButtonHover(index)}
+                      onMouseLeave={handleButtonLeave}
+                      className={activeButton === index ? 'active' : ''}
+                    >
+                      <BiHelpCircle/>
+                    </p>
+                {activeButton === index && (
+                  <div className="tooltip">
+                    <p>This client has a verified payment method with Upwork.</p>
+                  </div>
+                )}
                 </div>
               </div>
-        </div>
+            </div>
+          </div>
         </Link>
       ))
       }
