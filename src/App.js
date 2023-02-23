29b80d166c9a0299feef119f0ad3efbda11b88cb
eddiemuhoro <eddiemuhoro@gmail.com>
2023-02-23 21:49:00 +0300

    Add: addded white and dark theme

--- src/App.js
+++ src/App.js
@@ -19,16 +19,22 @@ import LoginEmployer from './components/authenticatioin/LoginEmployer';
 import Joinas from './components/authenticatioin/Joinas';
 import SingleJob from './components/homePage/categories/SingleJob';
 import Apply from './components/homePage/categories/Apply';
+import { useState } from 'react';
+import { ModeNight, WbSunny } from '@mui/icons-material';
 
 
 
 function App() {
+  const [theme, setTheme]= useState('white')
+  const handleClick = () => {
+    theme === 'white' ? setTheme('dark') : setTheme('white');
+  }
   return (
-    <div className="App">
-      
+    <div className= {theme === 'white' ? 'App' : 'dark'} >
+    <Button onClick={handleClick} style={{position:'absolute',zIndex:'999', bottom:"0%", right:'0%', backgroundColor: theme === 'white' ? 'white' : 'black',  color: theme === 'white' ? 'black' : 'white' }}>{theme === 'white'?<WbSunny/>: <ModeNight/>}</Button>
         <Routes>
           <Route path='/jobs' element={<><Mui /><Footer/>  </>} />
-          <Route path='/post' element={<> <Post /><Footer/>  </>} />
+          <Route path='/post' element={<> <Post /><Footer/>  </>} theme={theme} />
           <Route path='/' element={<><Home /><Footer/> </>} />
           <Route path='/aboutus' element={<><HomeNavBar/><AboutUs/><Footer/>  </>} />
           <Route path='/directors' element={<><HomeNavBar/><Team /><Footer/> </>} />
@@ -40,10 +46,7 @@ function App() {
           <Route path='/login' element={<><Login /></>} />
           <Route path='aboutus/instagram/jobsydekut' element={<SocialMedia />} />
           <Route path='/job/:id' element={<SingleJob />} />
-          
           <Route path='job/:id/apply' element={<Apply />} />
-          
-
         </Routes>
 
   </div>
