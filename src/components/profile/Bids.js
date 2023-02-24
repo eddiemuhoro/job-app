37b683f16f64dcdf019daefa6567c07ddead78f2
eddiemuhoro <eddiemuhoro@gmail.com>
2023-02-24 17:33:43 +0300

    CHANGE:changed to api to local API

--- src/components/profile/Bids.js
+++ src/components/profile/Bids.js
@@ -2,69 +2,74 @@ import React, { useEffect } from 'react'
 import axios from 'axios'
 import { useSelector } from 'react-redux'
 import Popup from 'reactjs-popup'
+import {BsFilterSquare} from 'react-icons/bs'
+import './profile.css'
 import { Button, Paper } from '@mui/material'
-const Bids = ({job}) => {
+const Bids = ({job, name}) => {
     const [bids, setBids] = React.useState([])
-    console.log(job);
+    console.log(`job:${job}`);
         
      useEffect(()=>{
-        axios.request(`https://fumbling-amusement-production.up.railway.app/bid/${job}`)
+        axios.request(`http://localhost:8000/bid/job/${job}`)
         .then((response) => {
             setBids(response.data);
-            console.log(response.data);
+            console.log(`data:${response.data}`);
         }
         ).catch((error) => {
             console.log(error);
         })
      },[])
-     const [isSelected , setisSelected] = React.useState(true)
-     const isPicked =(id)=>{
-        if(window.confirm('Are you sure you want to approve this bid?')){
-          axios.put(`https://fumbling-amusement-production.up.railway.app/bid/select/${id}`, isSelected)
-            .then((response) => {
-                setisSelected(response.data);
-                console.log(response.data);
-            }
+    
+        //hide and show bids
+        const [show, setShow] = React.useState(false)
+        const handleShow = () => {
+            setShow(!show)
+        }
 
-            ).catch((error) => {
-                console.log(error);
-            }
-            )
-        }
-        }
-        
-     
+        console.log(show);
+ 
     
     
   return (
     <div>
-        <h1>Bids</h1>
+        <button onClick={handleShow} style={{backgroundColor:'green', color:'white'}}>{show ? `Close`: `view bids`}</button>
+        <div className='filters' style={{display:show ? 'block' : 'none'}}>
+            <h3>Filters</h3>
+            <ul>
+                
+                <li>Delivery time</li>
+                <li>Rating</li>
+                <li>Price</li>
+
+
+            </ul>
+        </div>
         {bids.map((bid) =>  {return (
-            <div key={bid.id}>
-                <Paper sx={{position:'relative', padding:'10px', m:1}} elevation={4}>
+            <>
+            <div key={bid.id} style={{display:show ? 'block' : 'none'}}>
+                <Paper style={{backgroundColor:"#f5f5f5"}}   sx={{position:'relative', padding:'10px', m:1}} elevation={4} >
                      <div className=''>
-                     <div style={{position:'absolute', bottom:"20px", right: '5px', background:'blue', borderRadius:'10px', color:'white'}} className='job-selected'>
-                         {bid.isSelected ? <p >selected</p> : <p>Not selected</p>}
-                     </div>
-                     <main className='job-description'>
-                         
-                                <h2 style={{color:"black"}}>{bid.name}</h2>
-                         {/* <p>JavaScript offers a wide variety of array methods that make working with arrays simpler. This article demonstrated how to render data from a nested array using a map array method. Apart from map, there are also methods to help you push data to an array, concatenate two arrays, or even sort an array.</p> */}
-                         <p>{bid.description}</p>
-                     </main>
                      
+                     <header className='bid-header'>
+                            <img src='https://www.w3schools.com/howto/img_avatar.png' alt='avatar' className='avatar'/>
+                            <div className='bid-header-text'>
+                                <h3>{bid.name}</h3>
+                                <p>eddie@gmail.com</p>
+                            </div>
+                     </header>
                      <div>
-                        
-                         <div  className='job-time'>                         
-                        
-                      
-                         <Button variant='contained' onClick={()=> {isPicked(bid.id)}}>Pick</Button>
-                         </div>
-                        
+                            <p>Thank you for taking the time to read our proposal. I have more than 3 years of experience of PHP, Laravel, Node.js, React.js, Vue.js and ASP.NET. I am confident to complete work perfectly. I wanna discuss more for this project via chatting. Desperatly looking forward for your response.
+                                Chat
+                                Award
+                            </p>
+                     </div>
+                     <div style={{display:'flex', justifyContent:'flex-end'}} >
+                        <Button style={{backgroundColor:'green', }} variant='contained' >Award</Button>
                      </div>
                  </div>
              </Paper>
             </div>
+            </>
 
         )})}
     </div>
